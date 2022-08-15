a4f0212b711c444d4c6f9a2a1fc21f2402125cce
Daniel Müller <acct.d-mueller@mailbox.org>
2022-08-15 20:35:45 +1200

    Use variables directly

--- README.md
+++ README.md
@@ -9,5 +9,5 @@ NAME_MODULE=module
 # 2. Run script
 
 ```sh
-curl -s https://raw.githubusercontent.com/leuchtum/template-utils/main/setup.sh | bash -s $NAME_PROJECT $NAME_CONDA_ENV $NAME_MODULE
+curl -s https://raw.githubusercontent.com/leuchtum/template-utils/main/setup.sh | bash
  ```
--- setup.sh
+++ setup.sh
@@ -1,23 +1,23 @@
-git clone https://github.com/leuchtum/template.git $1
+git clone https://github.com/leuchtum/template.git $NAME_PROJECT
 
-cd $1
+cd $NAME_PROJECT
 
 rm -rf .git/
 
 cut -c2- .gitignore > cache_gitignore
 mv cache_gitignore .gitignore
 
-mv template_module $3
+mv template_module $NAME_MODULE
 
 chmod 777 dev_checks.sh
 
-conda create -y -n $2
-conda install -y -n $2 python
-conda install -y -n $2 --file requirements.dev
-conda install -y -n $2 --file requirements.prod
+conda create -y -n $NAME_CONDA_ENV
+conda install -y -n $NAME_CONDA_ENV python
+conda install -y -n $NAME_CONDA_ENV --file requirements.dev
+conda install -y -n $NAME_CONDA_ENV --file requirements.prod
 
 git init --initial-branch=main
 git add .
 git commit -m "Initial commit"
 
-echo "\n\nTo activate env run:\n\n\tconda activate $2\n\n"
+echo "\n\nTo activate env run:\n\n\tconda activate $NAME_CONDA_ENV\n\n"
