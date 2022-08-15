ee607952d415592c62781f37b3ab52a178fb7bbc
Daniel Müller <acct.d-mueller@mailbox.org>
2022-08-15 20:04:37 +1200

    dev check

--- post_clone_1.sh
+++ post_clone_1.sh
@@ -1,3 +1,16 @@
 cd $1
+
 rm -rf .git/
-cat .gitignore | cut -c2-100000 > .gitignore
+
+cut -c2- .gitignore > cache_gitignore
+mv cache_gitignore .gitignore
+
+mv template_module $3
+
+chmod 777 dev_checks.sh
+
+conda create -y -n $2
+conda install -y -n $2 --file requirements.dev
+conda install -y -n $2 --file requirements.prod
+
+git init --initial-branch=main
